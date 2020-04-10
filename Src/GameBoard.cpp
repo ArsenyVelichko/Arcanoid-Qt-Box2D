13c9536e62b6fc4ecae5196dc9b1f8a61cf30f00
ArsenyVelichko <ArsenyVelichko@users.noreply.github.com>
2020-04-10 05:12:51 +0300

    Changed moving block creation order

--- Src/GameBoard.cpp
+++ Src/GameBoard.cpp
@@ -75,7 +75,7 @@ void GameBoard::CreateBlocksGrid(const QRectF& box, std::istream& source) {
   _grid = new BlocksGrid(box, dimensions);
   Block::setScorer(_scorer);
 
-  for (int i = dimensions[1] - 1; i >= 0; i--) {
+  for (int i = 0; i < dimensions[1]; i++) {
     for (int j = 0; j < dimensions[0]; j++) {
 
       BlockType type;
